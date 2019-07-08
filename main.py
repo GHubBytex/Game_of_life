6798123da9bf54499e751011b07846908494f300
Vincent Hein <vincent.hein.de@gmail.com>
2019-07-08 16:18:40 +0200

    Zähle die Nachbarn [Funktioniert!]

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ def spielfeldAusgeben(Spielfeld):
 # Unsere hauptfunktion
 def main():
     Spielfeld = [
-        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 0],
         [0, 0, 1, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 0],
         ]
     spielfeldAusgeben(Spielfeld)
 
@@ -20,33 +20,30 @@ def main():
         for spalte in range(len(Spielfeld[zeile])):
             aktuelleZelle = Spielfeld[zeile][spalte]
             anzahlLebenderNachbarn = 0
+
+            # rechter nachbar
             if spalte + 1 < len(Spielfeld[zeile]):
                 anzahlLebenderNachbarn = anzahlLebenderNachbarn + Spielfeld[zeile + 0][spalte + 1]
-            if spalte + 1 < len(Spielfeld[zeile]):
+            # rechts unte
+            if spalte + 1 < len(Spielfeld[zeile]) and zeile + 1 < len(Spielfeld):
                 anzahlLebenderNachbarn = anzahlLebenderNachbarn + Spielfeld[zeile + 1][spalte + 1]
-            if spalte + 1 < len(Spielfeld[zeile]):
+            if zeile + 1 < len(Spielfeld):
                 anzahlLebenderNachbarn = anzahlLebenderNachbarn + Spielfeld[zeile + 1][spalte + 0]
-            if spalte + 1 < len(Spielfeld[zeile]):
+            if zeile + 1 < len(Spielfeld) and spalte - 1 >= 0:
                 anzahlLebenderNachbarn = anzahlLebenderNachbarn + Spielfeld[zeile + 1][spalte - 1]
-            if spalte + 1 < len(Spielfeld[zeile]):
+            if spalte - 1 >= 0:
                 anzahlLebenderNachbarn = anzahlLebenderNachbarn + Spielfeld[zeile + 0][spalte - 1]
-            if spalte + 1 < len(Spielfeld[zeile]):
+            if spalte - 1 >= 0 and zeile -1 >= 0:
                 anzahlLebenderNachbarn = anzahlLebenderNachbarn + Spielfeld[zeile - 1][spalte - 1]
-            if spalte + 1 < len(Spielfeld[zeile]):
+            if zeile - 1 >= 0:
                 anzahlLebenderNachbarn = anzahlLebenderNachbarn + Spielfeld[zeile - 1][spalte + 0]
-            if spalte + 1 < len(Spielfeld[zeile]):
+            if spalte + 1 < len(Spielfeld[zeile]) and zeile - 1 >= 0:
                 anzahlLebenderNachbarn = anzahlLebenderNachbarn + Spielfeld[zeile - 1][spalte + 1]
 
 
-
-
-
-
-            anzahlLebenderNachbarn = anzahlLebenderNachbarn + Spielfeld[1][0]
             print('Lebende nachbarn von [' + str(zeile) + ',' + str(spalte) + ']:' + str(anzahlLebenderNachbarn))
 
 
 
 if __name__ == '__main__':
     main()
-
